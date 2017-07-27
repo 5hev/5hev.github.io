5404b91ccebf5f4e65e6cea55789d3f506fdeb3e
5hev <s-h-e-v@mail.ru>
2017-07-27 18:09:18 +0300

    Update mystream.js

--- streamer/current/mystream.js
+++ streamer/current/mystream.js
@@ -57,10 +57,10 @@ var displayQuote = function(_quote) {
 	aEx.forEach (function(item,i,aEx){
 		
 		if (_quote.LASTMARKET == item) {
-			console.log(_quote);
+			//console.log(_quote);
 			document.getElementById(pair + "_" + item + "_change").innerHTML = _quote.CHANGEPCT24H.toFixed(2) + "%";
 			document.getElementById(pair + "_" + item + "_price").innerHTML = _quote.PRICE + ' (' + _quote.LASTMARKET + ')';
-			document.getElementById(pair + "_" + item + "_vol").innerHTML = _quote.VOLUME24HOUR;
+			document.getElementById(pair + "_" + item + "_vol").innerHTML = _quote.VOLUME24HOUR.toFixed(2);
 
 		if (_quote.FLAGS === "1"){
 			document.getElementById(pair + "_" + item + "_change").className = "up";
