a7591e1a4e9429226e2ea69e0bb682fa23f6b242
5hev <s-h-e-v@mail.ru>
2017-07-27 20:35:40 +0300

    Update mystream.js

--- streamer/current/mystream.js
+++ streamer/current/mystream.js
@@ -94,7 +94,7 @@ var socket = io.connect('https://streamer.cryptocompare.com/');
 //Format: {SubscriptionId}~{ExchangeName}~{FromSymbol}~{ToSymbol}
 //Use SubscriptionId 0 for TRADE, 2 for CURRENT and 5 for CURRENTAGG
 //For aggregate quote updates use CCCAGG as market
-var subscription = ['2~CCCAGG~ETH~BTC'];
+var subscription = ['5~CCCAGG~ETH~BTC'];
 
 socket.emit('SubAdd', {subs:subscription} );
 
