093d40022bae9cdf1c432a9a8c6977c795f06476
5hev <s-h-e-v@mail.ru>
2017-07-27 12:49:10 +0300

    Update mystream.js

--- streamer/current/mystream.js
+++ streamer/current/mystream.js
@@ -1,10 +1,17 @@
 var quote = {};
 
+var aEx = { "Poloniex", "Kraken" };
+
 var createDom = function(pair) {
 	var wrapper = document.getElementById("content");
 	var div = document.createElement("div");
 	var html = '<div class="wrapper">';
-	html += '<h1><span id="fsym_'+ pair +'"></span> - <span id="tsym_'+ pair +'"></span>   <strong><span class="price" id="price_'+ pair +'"></span></strong></h1>';
+	html += '<span id="fsym_'+ pair +'"></span> - <span id="tsym_'+ pair +'"></span>';
+	aEx.foreach(function(item,i,arr){
+		html += '<div class="price" id="price_'+ pair +'_'+ item +'"></div>';
+	});
+	html += '</div>';
+/*	
 	html += '<div class="label">24h Change: <span class="value" id="change_'+ pair +'"></span> (<span class="value" id="changepct_'+ pair +'"></span>)</div>';
 	html += '<div class="label">Last Market: <span class="market" id="market_'+ pair +'"></span></div>';
 	html += '<div class="label">Last Trade Id: <span class="value" id="tradeid_'+ pair +'"></span></div>';
@@ -14,6 +21,7 @@ var createDom = function(pair) {
 	html += '<div class="label">24h VolumeTo: <span class="value" id="24volumeto_'+ pair +'"></span></div>';
 	html += '<div class="source"> Source: <a href="http://www.cryptocompare.com">CryptoCompare</a></div>';
 	html += '</div>';
+*/	
 	div.innerHTML = html;
 	wrapper.appendChild(div);
 };
@@ -25,6 +33,7 @@ var displayQuote = function(_quote) {
 	var pair = _quote.FROMSYMBOL + _quote.TOSYMBOL;
 	console.log(_quote);
 	console.log(pair);
+/*	
 	document.getElementById("market_" + pair).innerHTML = _quote.LASTMARKET;
 	document.getElementById("fsym_" + pair).innerHTML = _quote.FROMSYMBOL;
 	document.getElementById("tsym_" + pair).innerHTML = _quote.TOSYMBOL;
@@ -37,7 +46,7 @@ var displayQuote = function(_quote) {
 	document.getElementById("tradeid_" + pair).innerHTML = _quote.LASTTRADEID.toFixed(0);
 	document.getElementById("change_" + pair).innerHTML = CCC.convertValueToDisplay(tsym, _quote.CHANGE24H);
 	document.getElementById("changepct_" + pair).innerHTML = _quote.CHANGEPCT24H.toFixed(2) + "%";
-
+*/
 	if (quote.FLAGS === "1"){
 		document.getElementById("price").className = "up";
 	} 
@@ -47,6 +56,12 @@ var displayQuote = function(_quote) {
 	else if (quote.FLAGS === "4") {
 		document.getElementById("price").className = "";
 	}
+	aEx.foreach (function(item,i,arr){
+		if (quote.LASTMARKET == item) {
+	   		document.getElementById("price_" + pair + "_" + item).innerHTML = _quote.PRICE;
+    		}
+	})
+	
 }
 
 var updateQuote = function(result) {
@@ -70,7 +85,7 @@ var socket = io.connect('https://streamer.cryptocompare.com/');
 //Format: {SubscriptionId}~{ExchangeName}~{FromSymbol}~{ToSymbol}
 //Use SubscriptionId 0 for TRADE, 2 for CURRENT and 5 for CURRENTAGG
 //For aggregate quote updates use CCCAGG as market
-var subscription = ['5~CCCAGG~BTC~USD','5~CCCAGG~ETH~USD'];
+var subscription = ['5~CCCAGG~BTC~USD'];
 
 socket.emit('SubAdd', {subs:subscription} );
 
